b2f084b78af83bea02aff9ed657810deb1fbb63b
CDConlyred <1746904635@qq.com>
2022-10-23 21:22:20 +0800

    根据买家/卖家的id查询其所有的订单

--- src/main/java/com/example/mall/controller/OrderController.java
+++ src/main/java/com/example/mall/controller/OrderController.java
@@ -1,7 +1,9 @@
 package com.example.mall.controller;
 
+import com.example.mall.entity.Good;
 import com.example.mall.entity.Order;
 import com.example.mall.service.OrderService;
+import org.apache.ibatis.ognl.ObjectElementsAccessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -39,4 +43,65 @@ public class OrderController {
         return model.toString();
 
     }
+
+    // 根据用户id查询其所有的订单
+    @RequestMapping(path = "/orderManage/buyer/getAll", method = RequestMethod.GET)
+    @ResponseBody
+    public String getAllOrdersWithBuyerId(Model model, String userId) {
+
+        int id = Integer.parseInt(userId);
+
+        List<Order> orders = orderService.findOrdersByUserId(id);
+
+        Map<String, Object> map = new HashMap<>();
+
+        for (Order order : orders) {
+            map.put("buyerId", order.getUserId());
+            map.put("shopId", order.getShopId());
+            map.put("goodsId", order.getGoodId());
+            map.put("orderNum", order.getPurchaseQuantity());
+            map.put("totalPrice", order.getTotalPrice());
+            map.put("goodsName", order.getGoodName());
+            map.put("goodsOption", order.getGoodOption());
+            map.put("orderStatus", order.getOrderStatus());
+            map.put("buyerAddress", order.getAddress());
+            map.put("buyerPhoneNumber", order.getTelNumber());
+            map.put("buyerName", order.getBuyerName());
+            model.addAttribute("orderId" + order.getOrderId(), map);
+        }
+
+        return model.toString();
+
+    }
+
+    // 根据店铺id查询其所有的订单
+    @RequestMapping(path = "/orderManage/seller/getAll", method = RequestMethod.GET)
+    @ResponseBody
+    public String getAllOrdersWithStoreId(Model model, String storeId) {
+
+        int id = Integer.parseInt(storeId);
+
+        List<Order> orders = orderService.findOrdersByStoreId(id);
+
+        Map<String, Object> map = new HashMap<>();
+
+        for (Order order : orders) {
+            map.put("buyerId", order.getUserId());
+            map.put("shopId", order.getShopId());
+            map.put("goodsId", order.getGoodId());
+            map.put("orderNum", order.getPurchaseQuantity());
+            map.put("totalPrice", order.getTotalPrice());
+            map.put("goodsName", order.getGoodName());
+            map.put("goodsOption", order.getGoodOption());
+            map.put("orderStatus", order.getOrderStatus());
+            map.put("buyerAddress", order.getAddress());
+            map.put("buyerPhoneNumber", order.getTelNumber());
+            map.put("buyerName", order.getBuyerName());
+            model.addAttribute("orderId" + order.getOrderId(), map);
+        }
+
+        return model.toString();
+
+    }
+
 }
--- src/main/java/com/example/mall/dao/OrderMapper.java
+++ src/main/java/com/example/mall/dao/OrderMapper.java
@@ -2,11 +2,20 @@ package com.example.mall.dao;
 
 import com.example.mall.entity.Order;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
 
     // order_id,user_id,shop_id,good_id,purchase_quantity,total_price,good_name,good_option,order_status,address,tel_number,buyer_name
     int insertOrder(Order order);
+
+    // 用买家id查询其所有的订单
+    List<Order> selectOrdersByUserId(@Param("userId") int userId);
+
+    // 用卖家id查询其所有的订单
+    List<Order> selectOrdersByShopId(@Param("shopId") int shopId);
 
 }
